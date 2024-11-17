b0ca4e6a7bef9ba563157fb56c1423762f062b37
nagi0705 <bellreuben1126@gmail.com>
2024-11-17 14:18:21 +0900

    15

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -3,7 +3,7 @@
 import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
 import React from 'react'
 import { Button } from './ui/button';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const EndCallButton = () => {
   const call = useCall();
@@ -12,16 +12,17 @@ const EndCallButton = () => {
   const { useLocalParticipant } = useCallStateHooks();
   const localParticipant = useLocalParticipant();
     
-  const isMeetingOwner = localParticipant &&
-  call?.state.createdBy && localParticipant.userId === call.state.createdBy.id
+    const isMeetingOwner = localParticipant &&
+    call?.state.createdBy && localParticipant.userId === call.state.createdBy.id;
     
   if (!isMeetingOwner) return null;
     
   return (
       <Button onClick={async () => {
           await call.endCall()
-    }}>
-          
+          router.push('/')
+    }} className="bg-red-500">
+      End call for everyone    
     </Button>
   )
 }
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,49 +1,42 @@
-import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
-import React, { useState, useEffect } from 'react';
+'use client';
+import { useState } from 'react';
+import {
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Users, LayoutList } from 'lucide-react';
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-import { cn } from '@/lib/utils';
-import { LayoutList, Users } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
+} from './ui/dropdown-menu';
+import Loader from './Loader';
 import EndCallButton from './EndCallButton';
+import { cn } from '@/lib/utils';
 
-type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
-type MeetingRoomProps = {
-  meetingId: string;
-};
-
-const MeetingRoom = ({ meetingId }: MeetingRoomProps) => {
+const MeetingRoom = () => {
   const searchParams = useSearchParams();
-  const isPersonalRoom = !!searchParams.get('personal')
+  const isPersonalRoom = !!searchParams.get('personal');
+  const router = useRouter();
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [showParticipants, setShowParticipants] = useState(false);
-  const [isMicAndCameraOff, setIsMicAndCameraOff] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
 
-  const handleCheckboxChange = () => {
-    setIsMicAndCameraOff((prevState) => !prevState);
-  };
+  // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
+  const callingState = useCallCallingState();
 
-  useEffect(() => {
-    if (isMicAndCameraOff) {
-      console.log('Mic and camera are turned OFF');
-      
-    } else {
-      console.log('Mic and camera are turned ON');
-    }
-  }, [isMicAndCameraOff]);
-
-  useEffect(() => {
-    
-  }, [meetingId]);
+  if (callingState !== CallingState.JOINED) return <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
@@ -59,7 +52,7 @@ const MeetingRoom = ({ meetingId }: MeetingRoomProps) => {
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
-        <div className="flex size-full max-w-[1000px] items-center">
+        <div className=" flex size-full max-w-[1000px] items-center">
           <CallLayout />
         </div>
         <div
@@ -70,52 +63,41 @@ const MeetingRoom = ({ meetingId }: MeetingRoomProps) => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-
+      {/* video layout and call controls */}
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
-        <div className="flex items-center">
-          <input
-            type="checkbox"
-            id="micCameraOff"
-            checked={isMicAndCameraOff}
-            onChange={handleCheckboxChange}
-            className="mr-2"
-          />
-          <label htmlFor="micCameraOff">Join with mic and camera off</label>
-        </div>
-        <CallControls />
-        
-        <DropdownMenu>
+        <CallControls onLeave={() => router.push(`/`)} />
 
+        <DropdownMenu>
           <div className="flex items-center">
-            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
               <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
           </div>
-          
           <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
             {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
               <div key={index}>
-                <DropdownMenuItem className="cursor-pointer"
-                  onClick={() => {
+                <DropdownMenuItem
+                  onClick={() =>
                     setLayout(item.toLowerCase() as CallLayoutType)
-                }}>
+                  }
+                >
                   {item}
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className="border-dark-1" />
               </div>
             ))}
           </DropdownMenuContent>
-      </DropdownMenu>
-      <CallStatsButton />
-      <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+        </DropdownMenu>
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
           </div>
         </button>
         {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default MeetingRoom;
